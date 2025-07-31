68e7bf0c5a8987a0aff06b4bea3ca181b43c5285
fatma873 <fatmafadila2015@gmail.com>
2025-07-31 09:22:38 +0700

    controller base

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-abstract class Controller
-{
-    //
-}
+class Controller extends BaseController // <-- Harus extend BaseController
+    {
+        use AuthorizesRequests, ValidatesRequests;
+    }
+
