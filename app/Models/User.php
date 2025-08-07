d1ad6ae0f41ed3b7996f36c33855c6774243a60c
fatma873 <fatmafadila2015@gmail.com>
2025-08-07 11:20:26 +0700

    gatau apa

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,12 +15,13 @@ class User extends Authenticatable implements MustVerifyEmail
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'role',
-    ];
+        protected $fillable = [
+            'name',
+            'email', 
+            'password',
+            'role',
+            'email_verified_at', 
+        ];
 
     /**
      * The attributes that should be hidden for arrays.
